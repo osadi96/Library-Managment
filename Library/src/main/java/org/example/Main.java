2383d10e1d7b8732c3fa680d7f4ce47ae52e1d97
osadi Chadeepani <osadichadeepani@gmail.com>
2024-02-03 18:54:56 +0530

    first commit

---
+++ Library/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void String (MysqlxDatatypes.Scalar.String[] args) {
+        SpringApplication.run(Main.class);
+    }
+}
