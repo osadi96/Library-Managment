15422726469ee70a7689d724115816f89f64844c
osadi Chadeepani <osadichadeepani@gmail.com>
2024-02-04 03:07:23 +0530

    add & get API done

--- Library/src/main/java/org/example/service/impl/BookServiceImpl.java
+++ Library/src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -1,19 +1,21 @@
 package org.example.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.example.dto.Book;
 import org.example.entity.BookEntity;
 import org.example.repository.BookRepository;
 import org.example.service.BookService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
 
-    @Autowired
-    BookRepository repository;
+    final BookRepository repository;
 
     ModelMapper mapper;
 
@@ -26,5 +28,10 @@ public class BookServiceImpl implements BookService {
     public void addBook(Book book) {
         BookEntity entity = mapper.map(book, BookEntity.class);
         repository.save(entity);
+    }
+
+    @Override
+    public List<BookEntity> getBooks(){
+        return (List<BookEntity>) repository.findAll();
     }
 }
