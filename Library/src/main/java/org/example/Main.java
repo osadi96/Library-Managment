bd6ca270140c3021438dbd0c8e16a9bb48d0165d
osadi Chadeepani <osadichadeepani@gmail.com>
2024-02-03 19:16:49 +0530

    Add book completed

--- Library/src/main/java/org/example/Main.java
+++ Library/src/main/java/org/example/Main.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-    public static void String (MysqlxDatatypes.Scalar.String[] args) {
+    public static void main (String[] args) {
         SpringApplication.run(Main.class);
     }
 }
