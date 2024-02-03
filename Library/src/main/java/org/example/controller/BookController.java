9907a270519afabdaeb172b9a66f82d8fe0ded3f
osadi Chadeepani <osadichadeepani@gmail.com>
2024-02-04 04:34:57 +0530

    delete API done

--- Library/src/main/java/org/example/controller/BookController.java
+++ Library/src/main/java/org/example/controller/BookController.java
@@ -1,19 +1,18 @@
 package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.Service;
 import org.example.dto.Book;
 import org.example.entity.BookEntity;
 import org.example.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/book")
 @RequiredArgsConstructor
 public class BookController {
-
-    final BookService service;
 
     @PostMapping("/add")
     @ResponseStatus (HttpStatus.CREATED)
@@ -26,7 +25,12 @@ public class BookController {
         return service.getBooks();
     }
 
-
-
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteBook(@PathVariable Long id) {
+        return service.deleteBook(id) ?
+                ResponseEntity.ok("Deleted!"):
+                ResponseEntity.notFound().build();
+    }
 
 }
--- Library/src/main/java/org/example/service/BookService.java
+++ Library/src/main/java/org/example/service/BookService.java
@@ -8,5 +8,8 @@ import java.util.List;
 public interface BookService {
     void addBook (Book book);
 
+    boolean deleteBook(Long id);
+
     List<BookEntity> getBooks();
+
 }
